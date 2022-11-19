317ca96f1c967f54e42babb2370d387b8bab2cef
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-19 14:03:02 -0600

    Further deployment fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/nodemcu'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/modemcu/index.html'));});
 app.listen(process.env.PORT || 8080);
