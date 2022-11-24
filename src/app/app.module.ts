2c6d371a38bd3f0c152e58a00b693874aa159794
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-23 20:55:22 -0600

    NGX ECHARTS removed temporarily

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
@@ -12,10 +11,7 @@ import { NgxEchartsModule } from 'ngx-echarts';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgxEchartsModule.forRoot({
-      echarts: () => import('echarts')
-    })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
