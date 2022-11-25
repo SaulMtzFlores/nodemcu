b6b1ad50bc4ac7539a537f63525bc5b657083b9d
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-24 23:36:58 -0600

    Update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,22 @@ export class AppComponent {
   options: any;
   constructor() { }
 
-  ngOnInit(): void {
-    const xAxisData:any = [];
-    const data1:any = [];
-    const data2:any = [];
+  async ngOnInit(): Promise<any> {
+    await this.addingLoop();
+  }
 
-    for (let i = 0; i < 100; i++) {
-      xAxisData.push('category' + i);
-      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    }
+  async addingLoop(temperatura:any=[], humedad:any=[], xAxisData:any=[]):Promise<any>{
+    console.log('Heee');
+
+    const values:any = this.getValues();
+    temperatura.push(values.temperatura);
+    humedad.push(values.humedad);
+    xAxisData.push(this.esdate());
+
 
     this.options = {
       legend: {
-        data: ['bar', 'bar2'],
+        data: ['Temperatura °C', 'Humedad %'],
         align: 'left',
       },
       tooltip: {},
@@ -37,20 +39,73 @@ export class AppComponent {
       yAxis: {},
       series: [
         {
-          name: 'bar',
+          name: 'Temperatura °C',
           type: 'bar',
-          data: data1,
+          data: temperatura,
           animationDelay: (idx) => idx * 10,
         },
         {
-          name: 'bar2',
+          name: 'Humedad %',
           type: 'bar',
-          data: data2,
+          data: humedad,
           animationDelay: (idx) => idx * 10 + 100,
         },
       ],
       animationEasing: 'elasticOut',
       animationDelayUpdate: (idx) => idx * 5,
     };
+
+    if(temperatura.length>=10){
+      temperatura.splice(0, 1);
+      humedad.splice(0, 1);
+      xAxisData.splice(0, 1);
+    }
+    setTimeout(async() => await this.addingLoop(temperatura, humedad, xAxisData), 1000)
+  }
+
+  esdate(showHour:boolean=true): string {
+    const dateObj: Date = new Date();
+    const dateInfo: any = {
+      date: dateObj,
+      day: dateObj.getDate(),
+      month: dateObj.getMonth(),
+      year: dateObj.getFullYear()
+    }
+
+    const date = `${dateInfo.day} de ${this.monthStr(dateInfo.month)} del ${dateInfo.year}`;
+    return (showHour) ? `${this.hourStr(dateInfo.date)}.` : `${date}`;
+  }
+
+
+  monthStr(month:number): string {
+    return [
+      'enero', 'febrero', 'marzo', 'abril',
+      'mayo', 'junio', 'julio', 'agosto',
+      'septiembre', 'octubre', 'noviembre', 'diciembre'
+    ][month];
+  }
+
+  hourStr(date: Date):string{
+    let hours = date.getHours();
+    let ampm= (hours >= 12) ? 'pm' : 'am';
+
+    hours = hours % 12;
+    hours = (hours) ? hours : 12;
+
+    let minutes: any = date.getMinutes();
+    let seconds: any = date.getSeconds();
+    let mill: any = date.getMilliseconds();
+
+    minutes = (minutes < 10) ? `0${minutes}`: minutes;
+    seconds = (seconds < 10) ? `0${seconds}`: seconds;
+
+    return `${hours}:${minutes}:${seconds} ${ampm}`;
+  }
+
+  getValues(){
+    return {
+      temperatura: 29 + ~~(Math.random() * (3 - (-3)) + (-3)),
+      humedad: 54 + ~~(Math.random() * (3 - (-3)) + (-3))
+    }
   }
 }
